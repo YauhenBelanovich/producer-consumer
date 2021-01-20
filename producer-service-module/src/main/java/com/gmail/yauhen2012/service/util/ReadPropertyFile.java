e48aa2029d4166e62b6b2ed3f9364c594db228b3
Yauhen Belanovich <yauhen2012@gmail.com>
2021-01-20 20:42:28 +0300

    basic auth for producer

--- producer-service-module/src/main/java/com/gmail/yauhen2012/service/util/ReadPropertyFile.java
+++ producer-service-module/src/main/java/com/gmail/yauhen2012/service/util/ReadPropertyFile.java
@@ -5,6 +5,8 @@ import java.io.InputStream;
 import java.lang.invoke.MethodHandles;
 import java.util.Properties;
 
+import com.gmail.yauhen2012.service.model.User;
+import com.gmail.yauhen2012.service.model.UserRoleEnum;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Component;
@@ -27,6 +29,24 @@ public class ReadPropertyFile {
         }
         logger.error("Cannot find bot property - " + "consumer.url");
         return null;
+    }
+
+    public User getUser(String userName) {
+        User user = new User();
+        try (InputStream inputStream = getClass().getResourceAsStream("/application.properties")) {
+            Properties prop = new Properties();
+            if (inputStream == null) {
+                logger.error("Sorry, unable to find application.properties");
+            }
+            prop.load(inputStream);
+            user.setName(prop.getProperty(userName + ".name"));
+            user.setRole(UserRoleEnum.valueOf(prop.getProperty(userName + ".role")));
+            user.setPassword(prop.getProperty(userName + ".password"));
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return user;
     }
 
 }
